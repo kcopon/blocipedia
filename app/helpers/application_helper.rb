9df32c281b7aa9b2992c37c76dfcf1f2a3768da1
Kris <justicecopon@gmail.com>
2015-09-27 07:09:06 -0700

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+def markdown_to_html(markdown)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true, hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true, gh_blockd: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render markdown).html_safe
 end
+
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
+  end    
+end
+
