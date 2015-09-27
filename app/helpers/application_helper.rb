814e3d9a92cfa68d5d9ddbb96d70ae0a6e8b57ab
Kris <justicecopon@gmail.com>
2015-09-27 07:07:52 -0700

    final commit for branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
-
 module ApplicationHelper
 
-  def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockd]
-    Redcarpet.new(text, *options).to_html.html_safe
-  end
+def markdown_to_html(markdown)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true, hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true, gh_blockd: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render markdown).html_safe
+end
 
   def syntax_highlighter(html)
     doc = Nokogiri::HTML(html)
@@ -14,3 +15,4 @@ module ApplicationHelper
     doc.to_s
   end    
 end
+
