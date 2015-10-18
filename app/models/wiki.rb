3457bca3c630510f9057b516687baa834e8931c4
Kris <justicecopon@gmail.com>
2015-10-18 00:53:26 -0700

    assignment submission for review 10/17

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,8 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+
+  #scope :visible_to, -> (user) { user ? all : where (public: true) }
+  #scope :publicly_viewable, -> { where(public: true) }
+  #scope :privately_viewable, -> { where(public: false) }
+
 end
